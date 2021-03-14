5d0dfb6852746551735bc2782abbfab43d9d9694
Michael Malto <michaeldm233@gmail.com>
2021-03-14 15:35:52 +0000

    refactor dir, added logo, bg image, login create account place holder

--- App.js
+++ App.js
@@ -1,37 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity, Dimensions, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import { useDeviceOrientation, useDimensions } from "@react-native-community/hooks";
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-
-  return (
-    <View style={{
-      backgroundColor: "white",
-      flex: 1, // grows to take the available free space, 1 = full, 0.5 half etc.. - default direction is down/ column
-      flexDirection: "row",
-    }}>
-      <View style={{ // shift + alt + down arrow to duplicate code in code
-        backgroundColor: "dodgerblue", // there is 4 segment from the child views, 2/4 segments are being taken up by dodger blue 
-        width: 100,
-        height: 100,          
-      }}/>
-      <View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100,    
-      }}/>
-      <View style={{
-        backgroundColor: "tomato",
-        width: 100,
-        height: 100,    
-      }}/>
-    </View>
-  );
+  console.log("App Start Success");
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, // will grow x, y to fill free space
-    backgroundColor: '#fff',
-  },
-}); 
