23ecf00db733aff7e4810b9d1cb26f0edf979162
Michael Malto <michaeldm233@gmail.com>
2021-03-14 12:51:11 +0000

    practise with views / styling / flexbox

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity, Dimensions, SafeAreaView } from 'react-native';
+import { useDeviceOrientation, useDimensions } from "@react-native-community/hooks";
 
 export default function App() {
 
-  console.log("App Started")
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </SafeAreaView>
+    <View style={{
+      backgroundColor: "white",
+      flex: 1, // grows to take the available free space, 1 = full, 0.5 half etc.. - default direction is down/ column
+      flexDirection: "row",
+    }}>
+      <View style={{ // shift + alt + down arrow to duplicate code in code
+        backgroundColor: "dodgerblue", // there is 4 segment from the child views, 2/4 segments are being taken up by dodger blue 
+        width: 100,
+        height: 100,          
+      }}/>
+      <View style={{
+        backgroundColor: "gold",
+        width: 100,
+        height: 100,    
+      }}/>
+      <View style={{
+        backgroundColor: "tomato",
+        width: 100,
+        height: 100,    
+      }}/>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1, // will grow x, y to fill free space
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
   },
 }); 
